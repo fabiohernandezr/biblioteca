fa2ea6f6301ddedfa3132696d413b102ee9f81b6
fabiohernandezr <fabiohernandezr@hotmail.com>
2019-10-28 11:30:31 -0500

    View Share

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Facade\FlareClient\View;
 use Illuminate\Support\ServiceProvider;
+use Facade\FlareClient\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('theme','lte');
+        View::share('theme', 'lte');
+        //share('theme','lte');
     }
 }
