8274fd63f7bb5b1114fa41ce9aa78abb10eff7f6
fabiohernandezr <fabiohernandezr@hotmail.com>
2019-10-28 11:00:05 -0500

    Creacion Plantillas Blade

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','InicioController');
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('foo', function(){
+ //   return'Hello word!';
+//});
 
-Route::get('foo', function(){
-    return'Hello word!';
-});
-
-Route::get('permiso/{nombre}/{slug}','PermisoController@index');
+//Route::get('permiso/{nombre}/{slug}','PermisoController@index');
 //Route::get('admin/sistema/permiso','PermisoController@index')->name('permiso'); rutas con nombre
 //Route::get('permiso','PermisoController@index');
 
